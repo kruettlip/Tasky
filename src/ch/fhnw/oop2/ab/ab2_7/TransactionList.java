456680e541d240044b3eb0d37858179b68aee1ac
kruettlip <philippe.kruettli@gmx.ch>
2022-03-15 13:09:36 +0100

    Remove unused import

--- src/ch/fhnw/oop2/ab/ab2_7/TransactionList.java
+++ src/ch/fhnw/oop2/ab/ab2_7/TransactionList.java
@@ -5,7 +5,6 @@ import static java.util.stream.Collectors.toList;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * This class implements a list of transactions performed by the traders over time.
